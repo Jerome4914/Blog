1e875cb3dd3757792457e7820325dd5e6f4c6fd0
jerome4914 <jeremyballew@hotmail.com>
2021-09-13 20:00:54 -0600

    added validations and images

--- main/forms.py
+++ main/forms.py
@@ -1,26 +1,46 @@
 from django import forms
-from .models import Post
+from .models import Post, Category
+
+# choices = [("uncategorized", "uncategorized"), ("sports", "sports"), ("entertainment", "entertainment"),] 
+# choices harcoded into search!!
+
+choices = Category.objects.all().values_list('name', 'name')
+choice_list = []
+for item in choices:
+    choice_list.append(item)
+
+class CategoryForm(forms.ModelForm):
+    class Meta:
+        model = Category
+        fields = ('name',)
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'})
+        }
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('title', 'title_tag', 'author', 'body')
+        fields = ('title', 'title_tag', 'author', 'category', 'body', 'snippet', 'header_image')
 
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control'}),
             'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
-            'author': forms.Select(attrs={'class': 'form-control'}),
+            'author': forms.TextInput(attrs={'class': 'form-control', 'value': '', 'id': 'user', 'type': 'hidden'}),
+            # 'author': forms.Select(attrs={'class': 'form-select'}),
+            'category': forms.Select(choices=choice_list, attrs={'class': 'form-select'}),
             'body': forms.Textarea(attrs={'class': 'form-control'}),
+            'snippet': forms.Textarea(attrs={'class': 'form-control'}),
         }
 
 class EditForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('title', 'title_tag', 'body')
+        fields = ('title', 'title_tag', 'body', 'snippet')
 
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control'}),
             'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
             # 'author': forms.Select(attrs={'class': 'form-control'}),
             'body': forms.Textarea(attrs={'class': 'form-control'}),
+            'snippet': forms.Textarea(attrs={'class': 'form-control'}),
         }
