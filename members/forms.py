b78ddaee756baad78f22257a8a4cb5eca7ff7c02
jerome4914 <jeremyballew@hotmail.com>
2021-10-02 01:05:46 -0600

    added user profile input and edit

--- members/forms.py
+++ members/forms.py
@@ -1,6 +1,45 @@
 from django.contrib.auth.forms import  UserCreationForm, UserChangeForm, PasswordChangeForm
 from django.contrib.auth.models import User
+from main.models import Profile
 from django import forms
+
+class EditProfilePageForm(forms.ModelForm):
+    bio = forms.CharField(widget=forms.Textarea(attrs={'class:': 'form-control'}))
+    # profile_pic = forms.ImageField(max_length=100, widget=forms.FileInput(attrs={'class:': 'form-control'}))
+    website_url = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class:': 'form-control'}))
+    facebook_url = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class:': 'form-control'}))
+    twitter_url = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class:': 'form-control'}))
+    instagram_url = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class:': 'form-control'}))
+    pintrest_url = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class:': 'form-control'}))
+
+    class Meta:
+        model = Profile
+        fields = ('bio', 'profile_pic', 'website_url', 'facebook_url', 'twitter_url', 'instagram_url', 'pintrest_url')
+        
+    def __init__(self, *args, **kwargs):
+        super(EditProfilePageForm, self).__init__(*args, **kwargs)
+        self.fields['bio'].widget.attrs['class'] = 'form-control'
+        self.fields['profile_pic'].widget.attrs['class'] = 'form-control'
+        self.fields['website_url'].widget.attrs['class'] = 'form-control'
+        self.fields['facebook_url'].widget.attrs['class'] = 'form-control'
+        self.fields['twitter_url'].widget.attrs['class'] = 'form-control'
+        self.fields['instagram_url'].widget.attrs['class'] = 'form-control'
+        self.fields['pintrest_url'].widget.attrs['class'] = 'form-control'
+
+class ProfilePageForm(forms.ModelForm):
+    class Meta:    
+        model = Profile
+        fields = ('bio', 'profile_pic', 'website_url', 'facebook_url', 'twitter_url', 'instagram_url', 'pintrest_url')
+
+        widgets = {
+            'bio': forms.Textarea(attrs={'class': 'form-control'}),
+            'profile_pic': forms.FileInput(attrs={'class': 'form-control'}),
+            'website_url': forms.TextInput(attrs={'class': 'form-control'}),
+            'facebook_url': forms.TextInput(attrs={'class': 'form-control'}),
+            'twitter_url': forms.TextInput(attrs={'class': 'form-control'}),
+            'instagram_url': forms.TextInput(attrs={'class': 'form-control'}),
+            'pintrest_url': forms.TextInput(attrs={'class': 'form-select'}),
+        }
 
 class SignUpForm(UserCreationForm):
     email = forms.EmailField(widget=forms.EmailInput(attrs={'class:': 'form-control'}))
